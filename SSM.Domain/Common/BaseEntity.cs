63f31719b5fa6cda01309b76709f082cd07dad22
Sled <ibrahimmirheshimov@gmail.com>
2026-09-09 11:15:41 +0400

    Last action: Token service added

---
+++ SSM.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SSM.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
